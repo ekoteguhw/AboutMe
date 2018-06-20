ceeb39eca1b0f3047c447b407105444f9cf9560f
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-06-20 22:46:16 +0700

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Eko Teguh Widodo on 20/06/18.
+//  Copyright © 2018 Eko Teguh Widodo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
